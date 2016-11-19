95baad8cade5ea406bd522865965b15855e2da98
Nobuyuki Iwanaga <ufcpp@live.jp>
2016-11-19 22:20:48 +0900

    remove Japanese comment

--- src/ContextFreeTasks.Shared/ContextFreeTask.cs
+++ src/ContextFreeTasks.Shared/ContextFreeTask.cs
@@ -3,11 +3,6 @@ using System.Threading.Tasks;
 
 namespace ContextFreeTasks
 {
-    /// <summary>
-    /// - 単なる <see cref="Task"/> クラスのラッパー
-    /// - 非同期メソッドの戻り値に使える。その場合、非同期メソッド内の await はすべて ConfigureAwait(false) 状態になる
-    /// - await できる。その await は ConfigureAwait(false) 状態になる
-    /// </summary>
     [AsyncMethodBuilder(typeof(AsyncContextFreeTaskMethodBuilder))]
     public struct ContextFreeTask
     {
--- src/ContextFreeTasks.Shared/ContextFreeTask_T.cs
+++ src/ContextFreeTasks.Shared/ContextFreeTask_T.cs
@@ -3,11 +3,6 @@ using System.Threading.Tasks;
 
 namespace ContextFreeTasks
 {
-    /// <summary>
-    /// - 単なる <see cref="Task{TResult}"/> クラスのラッパー
-    /// <see cref="ContextFreeTask"/>
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
     [AsyncMethodBuilder(typeof(AsyncContextFreeTaskMethodBuilder<>))]
     public struct ContextFreeTask<T>
     {
