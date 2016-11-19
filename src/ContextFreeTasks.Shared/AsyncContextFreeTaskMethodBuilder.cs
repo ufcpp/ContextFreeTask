1c86404e3d4420a91d9cc560a2ba6f2886ebb91e
Nobuyuki Iwanaga <ufcpp@live.jp>
2016-11-19 21:01:38 +0900

    src

---
+++ src/ContextFreeTasks.Shared/AsyncContextFreeTaskMethodBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Threading;
+
+namespace ContextFreeTasks
+{
+    [StructLayout(LayoutKind.Auto)]
+    public struct AsyncContextFreeTaskMethodBuilder
+    {
+        private AsyncTaskMethodBuilder _methodBuilder;
+        public static AsyncContextFreeTaskMethodBuilder Create() =>
+            new AsyncContextFreeTaskMethodBuilder() { _methodBuilder = AsyncTaskMethodBuilder.Create() };
+        public void Start<TStateMachine>(ref TStateMachine stateMachine) where TStateMachine : IAsyncStateMachine => _methodBuilder.Start(ref stateMachine);
+        public void SetStateMachine(IAsyncStateMachine stateMachine) => _methodBuilder.SetStateMachine(stateMachine);
+        public void SetResult() => _methodBuilder.SetResult();
+        public void SetException(Exception exception) => _methodBuilder.SetException(exception);
+        public ContextFreeTask Task => new ContextFreeTask(_methodBuilder.Task);
+        public void AwaitOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine)
+            where TAwaiter : INotifyCompletion
+            where TStateMachine : IAsyncStateMachine
+        {
+            SynchronizationContext.SetSynchronizationContext(null);
+            _methodBuilder.AwaitOnCompleted(ref awaiter, ref stateMachine);
+        }
+        [SecuritySafeCritical]
+        public void AwaitUnsafeOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine)
+            where TAwaiter : ICriticalNotifyCompletion
+            where TStateMachine : IAsyncStateMachine
+        {
+            SynchronizationContext.SetSynchronizationContext(null);
+            _methodBuilder.AwaitUnsafeOnCompleted(ref awaiter, ref stateMachine);
+        }
+    }
+}
---
+++ src/ContextFreeTasks.Shared/AsyncContextFreeTaskMethodBuilder_T.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Threading;
+
+namespace ContextFreeTasks
+{
+    [StructLayout(LayoutKind.Auto)]
+    public struct AsyncContextFreeTaskMethodBuilder<T>
+    {
+        private AsyncTaskMethodBuilder<T> _methodBuilder;
+        public static AsyncContextFreeTaskMethodBuilder<T> Create() =>
+            new AsyncContextFreeTaskMethodBuilder<T>() { _methodBuilder = AsyncTaskMethodBuilder<T>.Create() };
+        public void Start<TStateMachine>(ref TStateMachine stateMachine) where TStateMachine : IAsyncStateMachine => _methodBuilder.Start(ref stateMachine);
+        public void SetStateMachine(IAsyncStateMachine stateMachine) => _methodBuilder.SetStateMachine(stateMachine);
+        public void SetResult(T result) => _methodBuilder.SetResult(result);
+        public void SetException(Exception exception) => _methodBuilder.SetException(exception);
+        public ContextFreeTask<T> Task => new ContextFreeTask<T>(_methodBuilder.Task);
+        public void AwaitOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine)
+            where TAwaiter : INotifyCompletion
+            where TStateMachine : IAsyncStateMachine
+        {
+            SynchronizationContext.SetSynchronizationContext(null);
+            _methodBuilder.AwaitOnCompleted(ref awaiter, ref stateMachine);
+        }
+        [SecuritySafeCritical]
+        public void AwaitUnsafeOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine)
+            where TAwaiter : ICriticalNotifyCompletion
+            where TStateMachine : IAsyncStateMachine
+        {
+            SynchronizationContext.SetSynchronizationContext(null);
+            _methodBuilder.AwaitUnsafeOnCompleted(ref awaiter, ref stateMachine);
+        }
+    }
+}
