8c98ef6ac164a5002f54ced2c7230baa0739a79c
Nobuyuki Iwanaga <ufcpp@live.jp>
2016-11-19 21:50:53 +0900

    test Net45

---
+++ tests/UnitTestNet45/SingleThreadedSynchronizationContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace UnitTest
+{
+    class SingleThreadedSynchronizationContext : SynchronizationContext, IDisposable
+    {
+        private struct WorkItem
+        {
+            private readonly SendOrPostCallback _callback;
+            private readonly object _state;
+
+            public WorkItem(SendOrPostCallback callback, object state)
+            {
+                _callback = callback ?? throw new ArgumentNullException("callback");
+                _state = state;
+            }
+
+            public void Execute() => _callback(_state);
+        }
+
+        private readonly ConcurrentQueue<WorkItem> _workItems = new ConcurrentQueue<WorkItem>();
+        private bool _isStopped = false;
+        private readonly Thread _thread;
+
+        public SingleThreadedSynchronizationContext()
+        {
+            _thread = new Thread(Run);
+            SetSynchronizationContext(this);
+            _thread.Start();
+        }
+
+        public int ThreadId => _thread.ManagedThreadId;
+
+        private void Run()
+        {
+            SetSynchronizationContext(this);
+            while (!_isStopped)
+            {
+                while (!_workItems.IsEmpty)
+                {
+                    if (_workItems.TryDequeue(out var currentItem))
+                    {
+                        currentItem.Execute();
+                    }
+                }
+                Thread.Sleep(10);
+            }
+        }
+
+        public override void Post(SendOrPostCallback d, object state) => _workItems.Enqueue(new WorkItem(d, state));
+        public override void Send(SendOrPostCallback d, object state) => throw new NotSupportedException();
+
+        public void Dispose()
+        {
+            _isStopped = true;
+            _thread.Join();
+        }
+    }
+}
---
+++ tests/UnitTestNet45/UnitTest1.cs
@@ -0,0 +1,112 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ContextFreeTasks;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        SingleThreadedSynchronizationContext _syncContext;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _syncContext = new SingleThreadedSynchronizationContext();
+        }
+
+        [TestCleanup]
+        public void Creanup()
+        {
+            _syncContext.Dispose();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TestMethod1Async().Wait();
+        }
+
+        private async Task TestMethod1Async()
+        {
+            await Task.Run(() => { });
+            OnMainThread();
+            await A1(10);
+            OnMainThread();
+            await B1(10);
+            OnMainThread();
+        }
+
+        private void OnThread(int expectedThreadId)
+        {
+            var threadId = System.Threading.Thread.CurrentThread.ManagedThreadId;
+            Assert.AreEqual(expectedThreadId, threadId);
+        }
+
+        private void OnMainThread() => OnThread(_syncContext.ThreadId);
+
+        private void OnPoolThread()
+        {
+            var threadId = System.Threading.Thread.CurrentThread.ManagedThreadId;
+            Assert.AreNotEqual(_syncContext.ThreadId, threadId);
+        }
+
+        private async Task<string> A1(int n)
+        {
+            OnMainThread();
+            var s = await A2(n);
+            OnPoolThread();
+            await Task.Delay(100);
+            OnPoolThread();
+            await A3();
+            OnPoolThread();
+            return s;
+        }
+
+        private async ContextFreeTask<string> A2(int n)
+        {
+            await Task.Delay(100);
+            OnPoolThread();
+            await Task.Delay(100);
+            OnPoolThread();
+            await A3();
+            OnPoolThread();
+            return n.ToString();
+        }
+
+        private async ContextFreeTask A3()
+        {
+            await Task.Delay(100);
+            OnPoolThread();
+        }
+
+        private async Task<string> B1(int n)
+        {
+            OnMainThread();
+            var s = await B2(n);
+            OnMainThread();
+            await Task.Delay(100);
+            OnMainThread();
+            await B3();
+            OnMainThread();
+            return s;
+        }
+
+        private async Task<string> B2(int n)
+        {
+            await Task.Delay(100);
+            OnMainThread();
+            await Task.Delay(100);
+            OnMainThread();
+            await B3();
+            OnMainThread();
+            return n.ToString();
+        }
+
+        private async Task B3()
+        {
+            await Task.Delay(100);
+            OnMainThread();
+        }
+    }
+}
