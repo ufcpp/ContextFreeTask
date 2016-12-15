fc69347b567b44fa945ae29cccc245dc0f121d24
acple <silentsphere110@gmail.com>
2016-12-15 22:54:35 +0900

    use lambda-bodied constructor
    他の構造体がそうなってたので統一

--- src/ContextFreeTasks.Shared/ContextFreeTaskAwaiter.cs
+++ src/ContextFreeTasks.Shared/ContextFreeTaskAwaiter.cs
@@ -7,7 +7,7 @@ namespace ContextFreeTasks
     public struct ContextFreeTaskAwaiter : ICriticalNotifyCompletion
     {
         private readonly Task _value;
-        internal ContextFreeTaskAwaiter(Task value) { _value = value; }
+        internal ContextFreeTaskAwaiter(Task value) => _value = value;
         public bool IsCompleted => _value.IsCompleted;
         public void GetResult() => _value.GetAwaiter().GetResult();
         public void OnCompleted(Action continuation) => _value.ConfigureAwait(false).GetAwaiter().OnCompleted(continuation);
--- src/ContextFreeTasks.Shared/ContextFreeTaskAwaiter_T.cs
+++ src/ContextFreeTasks.Shared/ContextFreeTaskAwaiter_T.cs
@@ -7,7 +7,7 @@ namespace ContextFreeTasks
     public struct ContextFreeTaskAwaiter<T> : ICriticalNotifyCompletion
     {
         private readonly Task<T> _value;
-        internal ContextFreeTaskAwaiter(Task<T> value) { _value = value; }
+        internal ContextFreeTaskAwaiter(Task<T> value) => _value = value;
         public bool IsCompleted => _value.IsCompleted;
         public T GetResult() => _value.GetAwaiter().GetResult();
         public void OnCompleted(Action continuation) => _value.ConfigureAwait(false).GetAwaiter().OnCompleted(continuation);
