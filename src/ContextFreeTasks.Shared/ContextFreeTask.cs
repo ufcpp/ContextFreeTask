3cc1b45ecf5ca57457d8722fbf116cc1d4ec96af
acple <silentsphere110@gmail.com>
2016-12-15 22:32:57 +0900

    add 'ConfigureAwait' method
    ContextFreeTaskをawaitした時点でContextが強制的に捨てられてしまうと、UIイベントハンドラ内などコンテキストが必要な場面でContextFreeTaskを返すAPIを一切使用することができなくなってしまう。

--- src/ContextFreeTasks.Shared/ContextFreeTask.cs
+++ src/ContextFreeTasks.Shared/ContextFreeTask.cs
@@ -12,5 +12,6 @@ namespace ContextFreeTasks
         internal ContextFreeTask(Task t) => _task = t;
         public ContextFreeTaskAwaiter GetAwaiter() => new ContextFreeTaskAwaiter(Task);
         public void Wait() => _task?.Wait();
+        public ConfiguredTaskAwaitable ConfigureAwait(bool continueOnCapturedContext) => Task.ConfigureAwait(continueOnCapturedContext);
     }
 }
